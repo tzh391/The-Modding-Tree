2811b465efefdca59015aa6c7ed4d6f6a81456e3
pg132 <picogilman@gmail.com>
2022-08-02 10:24:49 -0700

    v.032

--- js/mod.js
+++ js/mod.js
@@ -12,7 +12,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.031",
+	num: "0.032",
 	name: "Respecced Reincarnation",
 }
 
@@ -29,13 +29,16 @@ function isEndgameRaw(){
 
 let changelog = `<h1>Changelog:</h1><br>
 	<br><h2 style='color: #DDDD00'>Endgame:</h2><br>
-		Reaching the endgame screen (updated at least as of v0.031)<br><br>
+		Reaching the endgame screen (updated at least as of v0.032)<br><br>
 	<br><h2 style='color: #00CC00'>Notes</h2><br>
 		- Versions will be vA.B.C<br>
 		- A will be big releases.<br>
 		- B will be each content patch.<br>
 		- C will be small patches without content (bug/wording fixes).<br><br><br>
 
+	<br><h3 style='color: #CC0000'>v0.032</h3><br>
+		- Added a Capybara upgrade.<br>
+		- Added a Duck milestone.<br>
 	<br><h3 style='color: #CC0000'>v0.031</h3><br>
 		- Balanced until 1e192 Ducks.<br>
 		- Added three Beaver upgrades and a Capybara upgrade.<br>
