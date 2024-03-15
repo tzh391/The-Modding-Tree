2397eca5929cf6ddabc946300c3f80293e25e645
pg132 <picogilman@gmail.com>
2024-03-14 20:08:03 -0700

    v.043 init3

--- js/utils/NumberFormating.js
+++ js/utils/NumberFormating.js
@@ -123,3 +123,20 @@ function invertOOM(x){
         
         return new Decimal(.1).pow(e).times(m)
 }
+
+function romanize (num){
+        if (isNaN(num)) return NaN;
+        let digits = String(+num).split("")
+        let key = [     "","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                        "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                        "","I","II","III","IV","V","VI","VII","VIII","IX"]
+        let roman = ""
+        let i = 3;
+        while (i--)
+                roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+        return Array(+digits.join("") + 1).join("M") + roman;
+}
+
+
+
+
