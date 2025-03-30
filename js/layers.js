815e78d2719a2e0116762db157121deaca291452
Irene <54173571+tzh391@users.noreply.github.com>
2025-03-30 12:46:31 -0700

    Add files via upload

--- js/layers.js
+++ js/layers.js
@@ -7515,6 +7515,7 @@ addLayer("sci", {
                                 if (amt.lte(2)) return new Decimal("1.352e107")
                                 if (amt.lte(3)) return new Decimal("1.22e154")
                                 if (amt.lte(4)) return new Decimal("1.53e160")
+                                if (amt.lte(5)) return new Decimal("3.74e166")
                                 return Decimal.tetrate(10, amt.div(300).plus(2.33))
                         },
                         unlocked(){
@@ -7564,6 +7565,7 @@ addLayer("sci", {
                                 if (amt.lte(5)) return new Decimal("3.10e147")
                                 if (amt.lte(6)) return new Decimal("1.00e153")
                                 if (amt.lte(7)) return new Decimal("6.85e159")
+                                if (amt.lte(8)) return new Decimal("1.51e168")
                                 return Decimal.tetrate(9, amt.pow(2).times(3).div(1e4).plus(2.3704))
                         },
                         unlocked(){
