785e08d04a66fce4b5d498cbdfe54659920fb0a7
Irene <54173571+tzh391@users.noreply.github.com>
2025-05-05 23:24:10 -0700

    Add files via upload

--- js/layers.js
+++ js/layers.js
@@ -49148,7 +49148,7 @@ addLayer("hu", {
                         fullDisplay(){
                                 let a = "<b>Moral</b>, <b>Immoral</b>, and <b>Good</b> and add .2 to the Plant cost exponent"
                                 let b = "Goal: 49,506,000 Plants"
-                                let c = "Reward: At 954 <i>Hual</i> levels and each 8th after halve its base"
+                                let c = "Reward: At 954 <i>Hual</i> levels and each 8th after halve its base (max 25)"
                                 c += " and at 1e62,263 / 1e62,353 / 1e62,491 / 1e62,664 / 1e62,882 / 1e63,050 / 1e63,450 Humans Mastery VII base is 3 / 2.5 / <br>2.2 / 2 / 1.9 / 1.8 / 1.7"
                                 if (hasMilestone("r", 4)) c = "Reward: Divide the <i>Hual</i> base cost by 2<sup>25</sup> and Mastery VII's base is 1.7"
                                 
